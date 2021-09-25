0d1fc22eca7f870a266ddb9ab700c93b791809ae
max anselmo <itmanselmo@gmail.com>
2021-09-25 08:52:55 -0400

    commit tree ae98845a1de2c93fb5f6ec6124ac59c8cd430445

--- index.js
+++ index.js
@@ -1,5 +1,5 @@
 // Your code goes here
 document.addEventListener('DOMContentLoaded', () => {
-    document.querySelector( 'p' ).innerText = 'This is really cool!'
+    document.querySelector( 'p' ).textContent = 'This is really cool!'
 })
 
